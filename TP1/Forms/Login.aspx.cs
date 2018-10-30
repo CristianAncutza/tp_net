9c6268fdd7101c7fe17fff1958c21315bddaa1dc
frios1306 <frios1306@gmail.com>
2018-10-29 23:40:48 -0300

    fabian
    Entity Framework hecho en clase

--- TP1/Forms/Login.aspx.cs
+++ TP1/Forms/Login.aspx.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TP1.Model;
 
 namespace TP1
 {
     public partial class Login: System.Web.UI.Page
     {
+        TP1Entities usuarioLogin = new TP1Entities();
+        usuario usu = new usuario();
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -20,25 +25,75 @@ namespace TP1
 
             if (Page.IsValid)
             {
-                SqlConnection sqlcon;
-                SqlCommand sqlcom;
 
-                sqlcon = new SqlConnection(@"Data Source=localhost ;Initial Catalog=tp1;Integrated Security=True;Pooling=False");
-                sqlcon.Open();
-
-                sqlcom = new SqlCommand("SELECT * FROM usuario WHERE usuario = '" + TxtUsuario.Text + "' AND password = '" + TxtPassword.Text + "'", sqlcon);
-                SqlDataReader dr = sqlcom.ExecuteReader();
-
-                if (dr.Read())
+                LblError.Visible = false;
+                if (!validarLogin())
                 {
-                    Session["UsuarioLogueado"] = TxtUsuario.Text;
-                    Response.Redirect("Alumno_lista.aspx");
+                    LblError.Visible = true;
                 }
                 else
                 {
-                    LblError.Text = "Verifique los datos ingresados.";
+                    LblError.Visible = false;
+
+
+
+
+                    FormsAuthentication.SetAuthCookie(usu.id.ToString(), false);
+                    Session["Usuario"] = usu;
+
+
+                    Response.Redirect("~/Forms/Alumno_lista.aspx", false);
+                    Context.ApplicationInstance.CompleteRequest();
+
+
                 }
-                sqlcon.Close();
+                /*
+
+            SqlConnection sqlcon;
+            SqlCommand sqlcom;
+
+            sqlcon = new SqlConnection(@"Data Source=localhost ;Initial Catalog=tp1;Integrated Security=True;Pooling=False");
+            sqlcon.Open();
+
+            sqlcom = new SqlCommand("SELECT * FROM usuario WHERE usuario = '" + TxtUsuario.Text + "' AND password = '" + TxtPassword.Text + "'", sqlcon);
+            SqlDataReader dr = sqlcom.ExecuteReader();
+
+            if (dr.Read())
+            {
+                Session["UsuarioLogueado"] = TxtUsuario.Text;
+                Response.Redirect("Alumno_lista.aspx");
+            }
+            else
+            {
+                LblError.Text = "Verifique los datos ingresados.";
+            }
+            sqlcon.Close();*/
+            }
+        }
+
+        private bool validarLogin() {
+
+            try
+            {
+                string usua = TxtUsuario.Text;
+                string pass = TxtPassword.Text;
+
+                var user = (from u in usuarioLogin.usuario where u.password == pass && u.usuario1 == usua select u).FirstOrDefault();
+
+                if (user != null)
+                {
+                    usu = user;
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
             }
         }
      }
